b6842157c18079eab0016f9a20afef039b5deee9
ngominh <ngominhth87@gmail.com>
2021-10-18 21:33:59 +0700

    may tinh

---
+++ may_tinh.js
@@ -0,0 +1,29 @@
+
+function click_add(value)
+{
+    document.getElementById("ket_qua").value +=value;
+  
+}
+
+function click_ketqua()
+{
+
+    document.getElementById("ket_qua").value=eval(document.getElementById("ket_qua").value);
+    //document.getElementById("ket_qua").innerHTML=tinh;
+}
+/*function click_bang()
+{
+    let mo_ngoac=document.getElementById("mo_ngoac").value;
+    let dong_ngoac=document.getElementById("dong_ngoac").value;
+    let phantram=document.getElementById("phan_tram").value;
+    let cong=document.getElementById("cong").value;
+
+    let tam=document.getElementById("tam").value;
+
+
+
+
+
+    //document.getElementById("ket_qua").innerHTML=eval();
+
+}*/
